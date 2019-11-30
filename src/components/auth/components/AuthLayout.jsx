ccb9a59b557ed4362e20999cb2305e3c450f3d4d
Gideon Nnalue <emekannalue@gmail.com>
2019-11-30 19:56:00 +0100

    fix auth layout git issues

---
+++ src/components/auth/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import logo from '../../../assets/img/logo.png';
+import { siteData } from '../constants';
+
+const authLeftInfo = () => (
+    <div className="l-authLayout__brand">
+        <img src={logo} alt="site logo" className="l-authLayout__img" />
+        <div className="l-authLayout__brandName">{siteData.siteNameText}</div>
+    </div>
+);
+
+const AuthLayout = props => {
+    return (
+        <Grid container className="l-authLayout">
+            <Grid item md={6} className="left">
+                {authLeftInfo()}
+                <div className="l-authLayout__brandDesc">
+                    <div>{siteData.siteDesc1Text}</div>
+                </div>
+            </Grid>
+            <Grid item md={6} className="right">
+                {props.children}
+            </Grid>
+        </Grid>
+    );
+};
+
+export default AuthLayout;
---
+++ src/components/auth/constants.js
@@ -0,0 +1,7 @@
+const siteData = {
+	siteNameText: 'health-e',
+	siteDesc1Text:
+		'Connect with friends around the world with great health topics',
+};
+
+export { siteData };
