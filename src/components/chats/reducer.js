9d991f31a4226acee6a95e3d5d7c5908cf4b770d
Gideon Nnalue <emekannalue@gmail.com>
2020-01-24 03:08:34 +0100

    [add] - chatting system

--- src/components/chats/reducer.js
+++ src/components/chats/reducer.js
@@ -1,7 +1,10 @@
-import { REFRESH_ROOMS } from './actionTypes';
+/* eslint-disable max-lines-per-function */
+import { REFRESH_ROOMS, CHANGE_ROOM, SET_MESSAGES } from './actionTypes';
 
 const INITIAL_STATE = {
 	rooms: [],
+	currentRoom: 0,
+	messages: [],
 	loading: false,
 };
 
@@ -11,6 +14,18 @@ export default (state = INITIAL_STATE, { type, payload }) => {
 			return {
 				...state,
 				rooms: payload,
+			};
+		}
+		case CHANGE_ROOM: {
+			return {
+				...state,
+				currentRoom: payload,
+			};
+		}
+		case SET_MESSAGES: {
+			return {
+				...state,
+				messages: payload,
 			};
 		}
 		default:
